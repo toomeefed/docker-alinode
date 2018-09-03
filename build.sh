d8780b617f3461e513501325769431c091504ab2
楼教主 <cik520@qq.com>
2018-09-03 17:35:58 +0800

    add build script

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ $1 = "3" ]; then
+  docker build -f 3/Dockerfile -t toomee/alinode:3 context
+  exit;
+fi
+
+if [ $1 = "3-slim" ]; then
+  docker build -f 3-slim/Dockerfile -t toomee/alinode:3-slim context
+  exit;
+fi
