a6dddb188e57429fe48ce60eb3fe242048bd5e8c
楼教主 <cik520@qq.com>
2018-09-11 11:26:25 +0800

    老版本编译

--- build.sh
+++ build.sh
@@ -9,3 +9,8 @@ if [ $1 = "3-slim" ]; then
   docker build -f 3-slim/Dockerfile -t toomee/alinode:3-slim context
   exit;
 fi
+
+if [ $1 = "3-tnvm" ]; then
+  docker build -f 3-tnvm/Dockerfile -t toomee/alinode:3-tnvm context
+  exit;
+fi
