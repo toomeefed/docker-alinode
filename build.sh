6d7d1ca7de70d6aa44d6933bbb9bdd09034d4120
楼教主 <cik520@qq.com>
2018-09-25 14:50:54 +0800

    build script

--- build.sh
+++ build.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
-if [ $1 = "3" ]; then
-  docker build -f 3/Dockerfile -t toomee/alinode:3 context
-  exit;
+set -e
+
+# 默认 tag 3
+tag=${1:-3}
+
+if [ ! -d $tag ]; then
+  echo "no such tag: $tag"
+  exit -1
 fi
 
-if [ $1 = "3-slim" ]; then
-  docker build -f 3-slim/Dockerfile -t toomee/alinode:3-slim context
-  exit;
-fi
+docker build -f $tag/Dockerfile -t toomee/alinode:$tag context
 
-if [ $1 = "3-tnvm" ]; then
-  docker build -f 3-tnvm/Dockerfile -t toomee/alinode:3-tnvm context
-  exit;
-fi
+echo
+echo "✨  toomee/alinode:$tag is done!"
+echo
+docker images toomee/alinode:$tag
+echo
+docker run --rm toomee/alinode:$tag sh -c 'echo "alinode v$ALINODE_VERSION" && echo "node $(node -v)" && echo "npm v$(npm -v)" && echo "yarn v$(yarn -v)" && echo "pm2 v$(pm2 -v)"'
+echo
